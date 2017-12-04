f50ae51a87cfb37fb27360c648bcf755f677e5a6
HallcSolutions <34248804+HallcSolutions@users.noreply.github.com>
2017-12-04 14:13:39 -0500

    Add files via upload

---
+++ Contador.php
@@ -0,0 +1,34 @@
+<?php
+ $n =1;
+ 
+ while ($n < 5) {
+    $n++;
+    
+    echo $n;
+            
+}
+
+
+echo "<br><br>";
+
+
+$p=1;
+
+do {
+    echo $p;
+            $p++;
+       
+}
+
+while ($p < 5);
+    
+    echo "<br><br>";
+
+
+for ($i = 0 ; $i < 5 ; $i++)
+{
+    echo $i;
+}
+
+
+?>
